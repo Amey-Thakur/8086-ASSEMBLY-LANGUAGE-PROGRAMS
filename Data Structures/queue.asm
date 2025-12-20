70aa94db2256e4d40d0b572d061efd8eae0214c8
Amey-Thakur <ameythakur20@gmail.com>
2025-12-20 18:27:25 -0500

    8086 Assembly Language (ASM)

--- Data Structures/queue.asm
+++ Data Structures/queue.asm
@@ -1,38 +1,63 @@
-; Program: Queue Implementation
-; Description: Implement a simple queue (FIFO) data structure
-; Author: Amey Thakur
+;=============================================================================
+; Program:     Queue Implementation (FIFO)
+; Description: Implement a circular queue data structure using an array.
+;              Demonstrates First-In-First-Out (FIFO) operations.
+; 
+; Author:      Amey Thakur
+; Repository:  https://github.com/Amey-Thakur/8086-ASSEMBLY-LANGUAGE-PROGRAMS
+; License:     MIT License
+;=============================================================================
 
 .MODEL SMALL
 .STACK 100H
 
+;-----------------------------------------------------------------------------
+; DATA SEGMENT
+;-----------------------------------------------------------------------------
 .DATA
-    QUEUE DB 10 DUP(0)
-    FRONT DW 0
-    REAR DW 0
-    SIZE DW 0
-    MAX_SIZE EQU 10
+    QUEUE DB 10 DUP(0)                  ; Queue array (10 elements)
+    FRONT DW 0                          ; Index of front element
+    REAR DW 0                           ; Index of next insertion point
+    SIZE DW 0                           ; Current number of elements
+    MAX_SIZE EQU 10                     ; Maximum queue capacity
+    
     MSG_FULL DB 'Queue is full!$'
     MSG_EMPTY DB 'Queue is empty!$'
     MSG_ENQUEUE DB 'Element added$'
     MSG_DEQUEUE DB 'Element removed$'
+    NEWLINE DB 0DH, 0AH, '$'
 
+;-----------------------------------------------------------------------------
+; CODE SEGMENT
+;-----------------------------------------------------------------------------
 .CODE
-; Enqueue Procedure
+
+;-----------------------------------------------------------------------------
+; ENQUEUE: Add element to rear of queue
 ; Input: AL = element to add
+;-----------------------------------------------------------------------------
 ENQUEUE PROC
+    ; Check if queue is full
     CMP SIZE, MAX_SIZE
     JE QUEUE_FULL
     
+    ; Add element at REAR position
     MOV BX, REAR
-    MOV QUEUE[BX], AL
-    INC REAR
+    MOV QUEUE[BX], AL                   ; Store element
+    INC REAR                            ; Move rear pointer
+    
+    ; Circular wrap-around
     CMP REAR, MAX_SIZE
     JL NO_WRAP_R
-    MOV REAR, 0      ; Circular wrap
+    MOV REAR, 0                         ; Wrap to beginning
 NO_WRAP_R:
-    INC SIZE
+    INC SIZE                            ; Increment count
     
+    ; Display confirmation
     LEA DX, MSG_ENQUEUE
+    MOV AH, 09H
+    INT 21H
+    LEA DX, NEWLINE
     MOV AH, 09H
     INT 21H
     RET
@@ -41,25 +66,38 @@ QUEUE_FULL:
     LEA DX, MSG_FULL
     MOV AH, 09H
     INT 21H
+    LEA DX, NEWLINE
+    MOV AH, 09H
+    INT 21H
     RET
 ENQUEUE ENDP
 
-; Dequeue Procedure
+;-----------------------------------------------------------------------------
+; DEQUEUE: Remove element from front of queue
 ; Output: AL = element removed
+;-----------------------------------------------------------------------------
 DEQUEUE PROC
+    ; Check if queue is empty
     CMP SIZE, 0
     JE QUEUE_EMPTY
     
+    ; Remove element from FRONT position
     MOV BX, FRONT
-    MOV AL, QUEUE[BX]
-    INC FRONT
+    MOV AL, QUEUE[BX]                   ; Get element
+    INC FRONT                           ; Move front pointer
+    
+    ; Circular wrap-around
     CMP FRONT, MAX_SIZE
     JL NO_WRAP_F
-    MOV FRONT, 0     ; Circular wrap
+    MOV FRONT, 0                        ; Wrap to beginning
 NO_WRAP_F:
-    DEC SIZE
+    DEC SIZE                            ; Decrement count
     
+    ; Display confirmation
     LEA DX, MSG_DEQUEUE
+    MOV AH, 09H
+    INT 21H
+    LEA DX, NEWLINE
     MOV AH, 09H
     INT 21H
     RET
@@ -68,14 +106,20 @@ QUEUE_EMPTY:
     LEA DX, MSG_EMPTY
     MOV AH, 09H
     INT 21H
+    LEA DX, NEWLINE
+    MOV AH, 09H
+    INT 21H
     RET
 DEQUEUE ENDP
 
+;-----------------------------------------------------------------------------
+; MAIN: Test queue operations
+;-----------------------------------------------------------------------------
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
     
-    ; Enqueue some elements
+    ; Enqueue elements: 10, 20, 30
     MOV AL, 10
     CALL ENQUEUE
     MOV AL, 20
@@ -83,10 +127,20 @@ MAIN PROC
     MOV AL, 30
     CALL ENQUEUE
     
-    ; Dequeue an element
+    ; Dequeue one element (removes 10)
     CALL DEQUEUE
     
-    MOV AH, 4CH      ; Exit to DOS
+    ; Exit to DOS
+    MOV AH, 4CH
     INT 21H
 MAIN ENDP
 END MAIN
+
+;=============================================================================
+; QUEUE (FIFO) DATA STRUCTURE:
+; - First-In-First-Out ordering
+; - ENQUEUE adds to REAR
+; - DEQUEUE removes from FRONT
+; - Circular implementation: rear wraps around to use freed space
+; - Applications: Task scheduling, printer queues, BFS traversal
+;=============================================================================
--- Data Structures/stack_array.asm
+++ Data Structures/stack_array.asm
@@ -1,31 +1,50 @@
-; Program: Stack Implementation (Using Array)
-; Description: Implement a stack data structure using an array
-; Author: Amey Thakur
+;=============================================================================
+; Program:     Stack Implementation (Using Array)
+; Description: Implement a stack data structure using an array.
+;              Demonstrates Last-In-First-Out (LIFO) operations.
+; 
+; Author:      Amey Thakur
+; Repository:  https://github.com/Amey-Thakur/8086-ASSEMBLY-LANGUAGE-PROGRAMS
+; License:     MIT License
+;=============================================================================
 
 .MODEL SMALL
 .STACK 100H
 
+;-----------------------------------------------------------------------------
+; DATA SEGMENT
+;-----------------------------------------------------------------------------
 .DATA
-    STACK_ARR DB 10 DUP(0)
-    TOP DW -1
-    MAX_SIZE EQU 10
+    STACK_ARR DB 10 DUP(0)              ; Stack array (10 elements)
+    TOP DW -1                           ; Top pointer (-1 = empty)
+    MAX_SIZE EQU 10                     ; Maximum stack capacity
+    
     MSG_PUSH DB 'Element pushed$'
     MSG_POP DB 'Element popped$'
     MSG_FULL DB 'Stack Overflow!$'
     MSG_EMPTY DB 'Stack Underflow!$'
     NEWLINE DB 0DH, 0AH, '$'
 
+;-----------------------------------------------------------------------------
+; CODE SEGMENT
+;-----------------------------------------------------------------------------
 .CODE
-; Push Procedure
+
+;-----------------------------------------------------------------------------
+; STACK_PUSH: Push element onto stack
 ; Input: AL = element to push
+;-----------------------------------------------------------------------------
 STACK_PUSH PROC
+    ; Check for overflow
     CMP TOP, MAX_SIZE - 1
     JGE STACK_OVERFLOW
     
+    ; Increment TOP and store element
     INC TOP
     MOV BX, TOP
-    MOV STACK_ARR[BX], AL
+    MOV STACK_ARR[BX], AL               ; Push element
     
+    ; Display confirmation
     LEA DX, MSG_PUSH
     MOV AH, 09H
     INT 21H
@@ -44,16 +63,21 @@ STACK_OVERFLOW:
     RET
 STACK_PUSH ENDP
 
-; Pop Procedure
+;-----------------------------------------------------------------------------
+; STACK_POP: Pop element from stack
 ; Output: AL = element popped
+;-----------------------------------------------------------------------------
 STACK_POP PROC
+    ; Check for underflow
     CMP TOP, -1
     JLE STACK_UNDERFLOW
     
+    ; Get element and decrement TOP
     MOV BX, TOP
-    MOV AL, STACK_ARR[BX]
+    MOV AL, STACK_ARR[BX]               ; Pop element
     DEC TOP
     
+    ; Display confirmation
     LEA DX, MSG_POP
     MOV AH, 09H
     INT 21H
@@ -72,11 +96,14 @@ STACK_UNDERFLOW:
     RET
 STACK_POP ENDP
 
+;-----------------------------------------------------------------------------
+; MAIN: Test stack operations
+;-----------------------------------------------------------------------------
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
     
-    ; Push elements
+    ; Push elements: 10, 20, 30
     MOV AL, 10
     CALL STACK_PUSH
     MOV AL, 20
@@ -84,10 +111,23 @@ MAIN PROC
     MOV AL, 30
     CALL STACK_PUSH
     
-    ; Pop element
+    ; Pop one element (removes 30 - last in, first out)
     CALL STACK_POP
     
-    MOV AH, 4CH      ; Exit to DOS
+    ; Exit to DOS
+    MOV AH, 4CH
     INT 21H
 MAIN ENDP
 END MAIN
+
+;=============================================================================
+; STACK (LIFO) DATA STRUCTURE:
+; - Last-In-First-Out ordering
+; - PUSH adds to TOP
+; - POP removes from TOP
+; - TOP = -1 indicates empty stack
+; - Applications: Expression evaluation, function calls, undo operations
+; 
+; Note: This is different from the 8086's hardware stack (SS:SP)
+;       This is an application-level stack using array
+;=============================================================================
