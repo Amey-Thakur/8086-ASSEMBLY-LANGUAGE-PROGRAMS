85273dc642edb67939eeb3112d0515ce1009d8f3
Amey-Thakur <ameythakur20@gmail.com>
2025-12-20 18:39:42 -0500

    8086 Assembly Language (ASM)

--- Graphics/draw_line.asm
+++ Graphics/draw_line.asm
@@ -1,55 +1,89 @@
-; Program: VGA Graphics - Draw Line
-; Description: Draw a horizontal line in VGA mode
-; Author: Amey Thakur
+;=============================================================================
+; Program:     VGA Line Drawing (Mode 13h)
+; Description: Demonstrate drawing a horizontal line in 256-color VGA mode
+;              by writing directly to video segment 0A000h.
+; 
+; Author:      Amey Thakur
+; Repository:  https://github.com/Amey-Thakur/8086-ASSEMBLY-LANGUAGE-PROGRAMS
+; License:     MIT License
+;=============================================================================
 
 .MODEL SMALL
 .STACK 100H
 
+;-----------------------------------------------------------------------------
+; DATA SEGMENT
+;-----------------------------------------------------------------------------
 .DATA
-    X_START DW 50
-    X_END DW 270
-    Y_POS DW 100
-    COLOR DB 14      ; Yellow
+    X_START DW 50                       ; Column start
+    X_END DW 270                        ; Column end
+    Y_POS DW 100                        ; Row position
+    COLOR DB 14                         ; Yellow color code
 
+;-----------------------------------------------------------------------------
+; CODE SEGMENT
+;-----------------------------------------------------------------------------
 .CODE
 MAIN PROC
+    ; Initialize Data Segment
     MOV AX, @DATA
     MOV DS, AX
     
-    ; Set VGA mode 13H
+    ;-------------------------------------------------------------------------
+    ; Set VGA Graphics Mode 13h
+    ; Resolution: 320x200 | Colors: 256
+    ; Video Segment: 0A000h
+    ;-------------------------------------------------------------------------
     MOV AH, 00H
     MOV AL, 13H
     INT 10H
     
-    ; Set ES to video segment
     MOV AX, 0A000H
-    MOV ES, AX
+    MOV ES, AX                          ; ES points to graphics memory
     
-    ; Calculate starting address
+    ;-------------------------------------------------------------------------
+    ; Calculate Linear Memory Address
+    ; Address = (Y * 320) + X
+    ;-------------------------------------------------------------------------
     MOV AX, Y_POS
     MOV BX, 320
-    MUL BX
-    ADD AX, X_START
-    MOV DI, AX
+    MUL BX                              ; AX = Y * 320
+    ADD AX, X_START                     ; AX = (Y * 320) + X_START
+    MOV DI, AX                          ; DI = Start pointer in video segment
     
-    ; Calculate line length
+    ;-------------------------------------------------------------------------
+    ; Calculate Line length (Pixels to Draw)
+    ;-------------------------------------------------------------------------
     MOV CX, X_END
-    SUB CX, X_START
+    SUB CX, X_START                     ; CX = Number of pixels
     
-    ; Draw horizontal line
+    ;-------------------------------------------------------------------------
+    ; Draw Horizontal Line
+    ; REP STOSB copies AL to ES:DI, incrementing DI, CX times.
+    ;-------------------------------------------------------------------------
     MOV AL, COLOR
+    CLD                                 ; Incrementing DI
     REP STOSB
     
     ; Wait for keypress
     MOV AH, 00H
     INT 16H
     
-    ; Return to text mode
+    ; Return to standard 80x25 text mode (Mode 03h)
     MOV AH, 00H
     MOV AL, 03H
     INT 10H
     
-    MOV AH, 4CH      ; Exit to DOS
+    ; Exit to DOS
+    MOV AH, 4CH
     INT 21H
 MAIN ENDP
 END MAIN
+
+;=============================================================================
+; VGA MODE 13H NOTES:
+; - Memory mapped at 0A000:0000 to 0A000:FA00 approx.
+; - Linear address space: 1 pixel = 1 byte.
+; - Total Pixels: 320 columns * 200 rows = 64,000 pixels.
+; - Color Palette: 256 colors defined by DAC registers.
+;=============================================================================
--- Graphics/draw_pixel.asm
+++ Graphics/draw_pixel.asm
@@ -1,51 +1,82 @@
-; Program: VGA Graphics - Draw Pixel
-; Description: Draw a single pixel in VGA mode 13H (320x200 256 colors)
-; Author: Amey Thakur
+;=============================================================================
+; Program:     VGA Pixel Drawing (Mode 13h)
+; Description: Draw a single pixel in the center of the screen in 
+;              320x200 256-color graphics mode.
+; 
+; Author:      Amey Thakur
+; Repository:  https://github.com/Amey-Thakur/8086-ASSEMBLY-LANGUAGE-PROGRAMS
+; License:     MIT License
+;=============================================================================
 
 .MODEL SMALL
 .STACK 100H
 
+;-----------------------------------------------------------------------------
+; DATA SEGMENT
+;-----------------------------------------------------------------------------
 .DATA
-    X_POS DW 160     ; Center X
-    Y_POS DW 100     ; Center Y
-    COLOR DB 15      ; White color
+    X_POS DW 160                        ; Center column (0-319)
+    Y_POS DW 100                        ; Center row (0-199)
+    COLOR DB 15                         ; White color code
 
+;-----------------------------------------------------------------------------
+; CODE SEGMENT
+;-----------------------------------------------------------------------------
 .CODE
 MAIN PROC
+    ; Initialize Data Segment
     MOV AX, @DATA
     MOV DS, AX
     
-    ; Set VGA mode 13H (320x200, 256 colors)
+    ;-------------------------------------------------------------------------
+    ; Set VGA Graphics Mode 13h
+    ; Resolution: 320x200 pixels
+    ; Colors: 256 simultaneous colors
+    ;-------------------------------------------------------------------------
     MOV AH, 00H
     MOV AL, 13H
     INT 10H
     
-    ; Calculate video memory address
-    ; Address = Y * 320 + X
+    ;-------------------------------------------------------------------------
+    ; Calculate Video Memory Address
+    ; Address = (Row * 320) + Column
+    ; Segment: 0A000h
+    ;-------------------------------------------------------------------------
     MOV AX, Y_POS
     MOV BX, 320
-    MUL BX           ; AX = Y * 320
-    ADD AX, X_POS    ; AX = Y * 320 + X
+    MUL BX                              ; AX = Y * 320
+    ADD AX, X_POS                       ; AX = (Y * 320) + X
     
     ; Set ES to video segment
     MOV BX, 0A000H
     MOV ES, BX
     
-    ; Draw pixel
-    MOV DI, AX
-    MOV AL, COLOR
-    MOV ES:[DI], AL
+    ;-------------------------------------------------------------------------
+    ; Plot the Pixel
+    ; Direct memory write to ES:[DI]
+    ;-------------------------------------------------------------------------
+    MOV DI, AX                          ; Offset in video memory
+    MOV AL, COLOR                       ; Get color index
+    MOV ES:[DI], AL                      ; Write to video memory
     
-    ; Wait for keypress
+    ; Wait for user input to see the result
     MOV AH, 00H
     INT 16H
     
-    ; Return to text mode
+    ; Reset back to standard text mode
     MOV AH, 00H
     MOV AL, 03H
     INT 10H
     
-    MOV AH, 4CH      ; Exit to DOS
+    ; Exit to DOS
+    MOV AH, 4CH
     INT 21H
 MAIN ENDP
 END MAIN
+
+;=============================================================================
+; VGA PIXEL ADDRESSING:
+; - Screen starts at (0,0) (Top-Left) and ends at (319,199) (Bottom-Right).
+; - Each byte in segment 0A000h represents one pixel.
+; - Color index 15 usually defaults to Bright White in the standard palette.
+;=============================================================================
