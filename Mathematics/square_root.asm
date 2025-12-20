9e5e55e9bc575fe532442f3ea2a6edf9b5bd788b
Amey-Thakur <ameythakur20@gmail.com>
2025-12-20 18:44:16 -0500

    8086 Assembly Language (ASM)

--- Mathematics/square_root.asm
+++ Mathematics/square_root.asm
@@ -1,42 +1,74 @@
-; Program: Square Root (Integer)
-; Description: Calculate integer square root of a number
-; Author: Amey Thakur
+;=============================================================================
+; Program:     Integer Square Root
+; Description: Calculate the integer part of the square root of a 16-bit
+;              unsigned number using the iterative 'Square Search' method.
+; 
+; Author:      Amey Thakur
+; Repository:  https://github.com/Amey-Thakur/8086-ASSEMBLY-LANGUAGE-PROGRAMS
+; License:     MIT License
+;=============================================================================
 
 .MODEL SMALL
 .STACK 100H
 
+;-----------------------------------------------------------------------------
+; DATA SEGMENT
+;-----------------------------------------------------------------------------
 .DATA
-    NUM DW 144       ; Number to find square root of
-    ROOT DW ?
-    MSG DB 'Square root calculated$'
+    NUM DW 144                          ; Input: Find sqrt of 144
+    ROOT DW ?                           ; Storage for final result (12)
+    MSG DB 'Integer Square Root calculated.$'
 
+;-----------------------------------------------------------------------------
+; CODE SEGMENT
+;-----------------------------------------------------------------------------
 .CODE
 MAIN PROC
+    ; Initialize DS
     MOV AX, @DATA
     MOV DS, AX
     
-    MOV BX, NUM
-    MOV CX, 1        ; Starting guess
+    MOV BX, NUM                         ; BX stores the target number
+    MOV CX, 1                           ; Iterative counter 'n'
     
-    ; Newton's method for integer square root
-    ; Or simple iteration: find largest n where n*n <= NUM
+;-------------------------------------------------------------------------
+; ITERATIVE SEARCH: Find largest n where (n * n) <= target
+;-------------------------------------------------------------------------
 FIND_ROOT:
     MOV AX, CX
-    MUL CX           ; AX = CX * CX
+    MUL CX                              ; AX = CX^2
+    
+    ; Check if we exceeded the target
     CMP AX, BX
-    JA FOUND         ; If CX*CX > NUM, previous was the answer
-    MOV ROOT, CX     ; Save current value
-    INC CX
+    JA TARGET_EXCEEDED                 ; If n^2 > NUM, (n-1) was the root
+    
+    MOV ROOT, CX                        ; Save current valid integer root
+    INC CX                              ; Test next value
+    
+    ; Prevent infinite loop for large numbers (CX > 255 for 16-bit range)
+    CMP CX, 256
+    JE TARGET_EXCEEDED
+    
     JMP FIND_ROOT
     
-FOUND:
-    ; ROOT now contains square root of 144 = 12
-    
+TARGET_EXCEEDED:
+    ; Status display
     LEA DX, MSG
     MOV AH, 09H
     INT 21H
     
-    MOV AH, 4CH      ; Exit to DOS
+    ; Result is now in ROOT memory location
+    
+    ; Exit
+    MOV AH, 4CH
     INT 21H
 MAIN ENDP
 END MAIN
+
+;=============================================================================
+; SQUARE ROOT NOTES:
+; - This is an O(sqrt(N)) algorithm.
+; - For a 16-bit input (max 65535), the max possible root is 255.
+; - More efficient algorithms like "Newton-Raphson" or "Binary Search" 
+;   exist for large ranges, but this linear search is simplest for 8086 logic.
+;=============================================================================
