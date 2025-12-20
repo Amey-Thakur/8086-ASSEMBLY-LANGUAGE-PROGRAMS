e5f49e9b435f6859c7ab5a4daf1e2861304fc4b1
Amey-Thakur <ameythakur20@gmail.com>
2025-12-20 18:39:42 -0500

    8086 Assembly Language (ASM)

--- Graphics/colored_text.asm
+++ Graphics/colored_text.asm
@@ -1,63 +1,92 @@
-; Program: Text Mode - Colored Text
-; Description: Display colored text in text mode
-; Author: Amey Thakur
+;=============================================================================
+; Program:     Colored Text Demo (Text Mode)
+; Description: Demonstrate how to display colored text by directly writing
+;              to the video memory segment 0B800h.
+; 
+; Author:      Amey Thakur
+; Repository:  https://github.com/Amey-Thakur/8086-ASSEMBLY-LANGUAGE-PROGRAMS
+; License:     MIT License
+;=============================================================================
 
 .MODEL SMALL
 .STACK 100H
 
+;-----------------------------------------------------------------------------
+; DATA SEGMENT
+;-----------------------------------------------------------------------------
 .DATA
-    MSG DB 'Colored Text Demo!'
+    MSG DB 'Colored Text Demo!'         ; String to display
+    LEN EQU 18                           ; String length
 
+;-----------------------------------------------------------------------------
+; CODE SEGMENT
+;-----------------------------------------------------------------------------
 .CODE
 MAIN PROC
+    ; Initialize Data Segment
     MOV AX, @DATA
     MOV DS, AX
     
-    ; Set text mode 80x25
+    ;-------------------------------------------------------------------------
+    ; Set Video Mode: Standard 80x25 Text Mode (Mode 03h)
+    ;-------------------------------------------------------------------------
     MOV AH, 00H
     MOV AL, 03H
     INT 10H
     
-    ; Set ES to video segment (text mode)
+    ;-------------------------------------------------------------------------
+    ; Access Video Memory
+    ; Segment 0B800h is the start of video memory in color text modes.
+    ; Each character on screen takes 2 bytes: [Char][Attribute]
+    ;-------------------------------------------------------------------------
     MOV AX, 0B800H
     MOV ES, AX
     
-    ; Position: row 12, column 30
-    ; Offset = (row * 80 + column) * 2
+    ; Calculate starting address: Row 12, Column 30
+    ; Memory Offset = (row * 80 + column) * 2
     MOV DI, (12 * 80 + 30) * 2
     
-    ; Display string with different colors
+    ; Setup for loop
     LEA SI, MSG
-    MOV CX, 18       ; String length
-    MOV BL, 0        ; Color counter
+    MOV CX, LEN                         ; Number of characters
+    MOV BL, 0                           ; Starting color counter
     
+;-------------------------------------------------------------------------
+; DISPLAY LOOP: Write Char and Attribute pair to ES:DI
+;-------------------------------------------------------------------------
 PRINT_LOOP:
-    LODSB            ; Get character
-    MOV ES:[DI], AL  ; Store character
+    LODSB                               ; Load character from DS:SI into AL
+    MOV ES:[DI], AL                     ; Store ASCII character in video memory
     INC DI
     
-    ; Set color (foreground + background)
-    ; Bits 0-3: foreground, Bits 4-6: background, Bit 7: blink
+    ; Attribute Byte Structure:
+    ; Bit 0-3: Foreground color
+    ; Bit 4-6: Background color
+    ; Bit 7  : Blink (if enabled)
     MOV AL, BL
-    AND AL, 0FH      ; Use only foreground colors (0-15)
-    OR AL, 10H       ; Blue background
-    MOV ES:[DI], AL
+    AND AL, 0FH                         ; Use foreground colors (0-15)
+    OR AL, 10H                          ; Add Blue background (bit 4=1)
+    
+    MOV ES:[DI], AL                     ; Store attribute in video memory
     INC DI
     
-    INC BL           ; Next color
+    INC BL                              ; Cycle to next color
     LOOP PRINT_LOOP
     
-    ; Wait for keypress
+    ; Wait for user keypress before exit
     MOV AH, 00H
     INT 16H
     
-    MOV AH, 4CH      ; Exit to DOS
+    ; Exit to DOS
+    MOV AH, 4CH
     INT 21H
 MAIN ENDP
 END MAIN
 
-; Color Reference:
-; 0 = Black,  1 = Blue,    2 = Green,   3 = Cyan
-; 4 = Red,    5 = Magenta, 6 = Brown,   7 = Light Gray
-; 8 = Dark Gray, 9 = Light Blue, 10 = Light Green, 11 = Light Cyan
-; 12 = Light Red, 13 = Light Magenta, 14 = Yellow, 15 = White
+;=============================================================================
+; TEXT MODE COLOR REFERENCE (Standard 16 Colors):
+; 0: Black      4: Red          8: Dark Grey    12: Light Red
+; 1: Blue       5: Magenta      9: Light Blue   13: Light Magenta
+; 2: Green      6: Brown        10: Light Green  14: Yellow
+; 3: Cyan       7: Light Grey   11: Light Cyan   15: White
+;=============================================================================
--- Graphics/draw_rectangle.asm
+++ Graphics/draw_rectangle.asm
@@ -1,63 +1,87 @@
-; Program: VGA Graphics - Draw Rectangle
-; Description: Draw a filled rectangle in VGA mode
-; Author: Amey Thakur
+;=============================================================================
+; Program:     VGA Rectangle Drawing (Mode 13h)
+; Description: Draw a filled rectangle in VGA mode by iterating through
+;              rows and using REP STOSB for efficient filling.
+; 
+; Author:      Amey Thakur
+; Repository:  https://github.com/Amey-Thakur/8086-ASSEMBLY-LANGUAGE-PROGRAMS
+; License:     MIT License
+;=============================================================================
 
 .MODEL SMALL
 .STACK 100H
 
+;-----------------------------------------------------------------------------
+; DATA SEGMENT
+;-----------------------------------------------------------------------------
 .DATA
-    X1 DW 50         ; Left
-    Y1 DW 50         ; Top
-    X2 DW 150        ; Right
-    Y2 DW 100        ; Bottom
-    COLOR DB 12      ; Light Red
+    X1 DW 50                            ; Left column
+    Y1 DW 50                            ; Top row
+    X2 DW 150                           ; Right column
+    Y2 DW 100                           ; Bottom row
+    COLOR DB 12                         ; Light Red color code
 
+;-----------------------------------------------------------------------------
+; CODE SEGMENT
+;-----------------------------------------------------------------------------
 .CODE
 MAIN PROC
+    ; Initialize Data Segment
     MOV AX, @DATA
     MOV DS, AX
     
-    ; Set VGA mode 13H
+    ;-------------------------------------------------------------------------
+    ; Set VGA Graphics Mode 13h
+    ;-------------------------------------------------------------------------
     MOV AH, 00H
     MOV AL, 13H
     INT 10H
     
-    ; Set ES to video segment
+    ; Set ES to video segment (0A000h)
     MOV AX, 0A000H
     MOV ES, AX
     
-    ; Calculate height
+    ;-------------------------------------------------------------------------
+    ; Calculate Dimensions
+    ;-------------------------------------------------------------------------
     MOV DX, Y2
-    SUB DX, Y1       ; DX = height
+    SUB DX, Y1                          ; DX = Height (number of rows)
     
-    ; Calculate width
     MOV BX, X2
-    SUB BX, X1       ; BX = width
+    SUB BX, X1                          ; BX = Width (pixels per row)
     
-    ; Starting Y position
+    ; Start at first row
     MOV AX, Y1
     
+;-------------------------------------------------------------------------
+; MAIN DRAWING LOOP: Iterate through rows
+;-------------------------------------------------------------------------
 DRAW_ROW:
     PUSH AX
-    PUSH DX
+    PUSH DX                             ; Save row counter
     
-    ; Calculate row starting address
+    ; Calculate offset for current row: (Y * 320) + X1
     MOV DX, 320
-    MUL DX
-    ADD AX, X1
-    MOV DI, AX
+    MUL DX                              ; DX:AX = Y * 320
+    ADD AX, X1                          ; AX = start of current row
+    MOV DI, AX                          ; DI points to row start in ES
     
-    ; Draw one row
-    MOV CX, BX
-    MOV AL, COLOR
-    REP STOSB
+    ; Draw the row scanline
+    MOV CX, BX                          ; Width of rectangle
+    MOV AL, COLOR                       ; Fill color
+    CLD                                 ; Increment DI
+    REP STOSB                           ; Write row pixels
     
-    POP DX
-    POP AX
-    INC AX           ; Next row
-    DEC DX
-    JNZ DRAW_ROW
+    POP DX                              ; Restore row counter
+    POP AX                              ; Restore current Y
     
+    INC AX                              ; Move to next row
+    DEC DX                              ; Decrement height counter
+    JNZ DRAW_ROW                        ; Continue until height=0
+    
+    ;-------------------------------------------------------------------------
+    ; Cleanup and Exit
+    ;-------------------------------------------------------------------------
     ; Wait for keypress
     MOV AH, 00H
     INT 16H
@@ -67,7 +91,15 @@ DRAW_ROW:
     MOV AL, 03H
     INT 10H
     
-    MOV AH, 4CH      ; Exit to DOS
+    ; Exit to DOS
+    MOV AH, 4CH
     INT 21H
 MAIN ENDP
 END MAIN
+
+;=============================================================================
+; RECTANGLE DRAWING NOTES:
+; - Instead of plotting pixel-by-pixel, we use 'REP STOSB' for each row.
+; - This is significantly faster as it utilizes dedicated string instructions.
+; - To draw an outlined rectangle, one would draw four individual lines.
+;=============================================================================
